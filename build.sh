70b1668db2eca3d47fa68878f98ea524b587e2df
Shom Bandopadhaya <shom@bandopadhaya.com>
2022-05-30 11:35:02 -0500

    Copy images for bundles, remove gps

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
+# Generate Hugo markdown using ox-hugo
 emacs -Q --script build.el
+
+echo ":\n ==== Process images and remove GPS info ===="
 cp -r images/* content/posts/
-echo "\n==== Build with Hugo ===="
+find . -name '*.jpg' -exec exiftool -GPS:All= {}
+
+echo ":\n ==== Build with Hugo ===="
 hugo
