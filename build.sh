3d0128189736a5c50a645e96d93480242fe4e978
Shom Bandopadhaya <shom@bandopadhaya.com>
2023-12-06 18:32:47 -0600

    film folder that will get copied to content, template deleted

--- build.sh
+++ build.sh
@@ -7,6 +7,10 @@ echo ":\n ==== Process images and remove GPS info ===="
 LC_ALL=C ; find ./images/ ./static -name '*.jpg' -exec exiftool -overwrite_original "-gps*=" {} \;
 cp -r images/* content/posts/
 
+echo ":\n ==== Copy film photo posts into content folder ===="
+cp -r ./film ./content/
+rm -rf ./content/film/.template
+
 echo ":\n ==== Build with Hugo ===="
 hugo
 
---
+++ film/.template/index.md
@@ -0,0 +1,21 @@
+---
+title: "Title"
+date: "2023-09-28"
+description: "Alt-text"
+tags:
+- FilmPhotography
+- BlackAndWhite
+- Color
+- MediumFormat
+- 35mm
+- CanonQL17Giii
+- Olympus35RC
+- Kiev60
+- Rolleiflex3.5Standard
+resources:
+- title: Title
+  src: 'image.jpg'
+---
+
+## Heading
+Text
