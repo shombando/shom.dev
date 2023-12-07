e293027e4893c8971653af6d8b0c88cfb004b706
Shom Bandopadhaya <shom@bandopadhaya.com>
2023-12-06 18:45:28 -0600

    Update build script and menu to accommodate film section

--- build.sh
+++ build.sh
@@ -3,15 +3,20 @@ set -euo pipefail
 # Generate Hugo markdown using ox-hugo
 emacs -Q --script build.el
 
-echo ":\n ==== Process images and remove GPS info ===="
+echo "==== Process images and remove GPS info ===="
 LC_ALL=C ; find ./images/ ./static -name '*.jpg' -exec exiftool -overwrite_original "-gps*=" {} \;
 cp -r images/* content/posts/
+echo ""
 
-echo ":\n ==== Copy film photo posts into content folder ===="
+echo "==== Copy film photo posts into content folder ===="
 cp -r ./film ./content/
 rm -rf ./content/film/.template
+echo ""
 
-echo ":\n ==== Build with Hugo ===="
+echo "==== Build with Hugo ===="
 hugo
+echo ""
 
+echo "==== Copy .well-known for webfinger ===="
 cp ./.well-known -r ./public/
+echo ""
