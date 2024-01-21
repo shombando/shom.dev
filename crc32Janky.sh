3bc2d0bd12aee39da65aefb655550a7a821afc02
Shom Bandopadhaya <shom@bandopadhaya.com>
2024-01-21 02:24:19 -0600

    Added short url support and wrote a post about it.

---
+++ crc32Janky.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/sh 
+
+# Returns the CRC32 hash of the string input passed in as the first argument
+# This is janky, send angry emails to: https://stackoverflow.com/questions/44804668/how-to-calculate-crc32-checksum-from-a-string-on-linux-bash#49446525
+echo -n $1 | gzip -1 -c | tail -c8 | hexdump -n4 -e '"%08x"'
