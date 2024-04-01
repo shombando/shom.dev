1237aa313fc3c2b87c37a0512e63bc2349c46f61
Shom Bandopadhaya <shom@bandopadhaya.com>
2024-03-31 21:24:47 -0500

    NixOS build attempt

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }: 
+    let pkgs = import nixpkgs { system = "x86_64-linux";
+                                config.allowUnfree = true;
+                                overlays = [];
+                              }; 
+    in {
+    
+      devShell.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.mkShell {
+        nativeBuildInputs = with pkgs.buildPackages; [
+          hugo
+		  exiftool
+		  emacs-nox
+        ];
+      };
+    };
+}
