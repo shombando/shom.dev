b8417c86bc39076a31778da00f52a5983b792006
Shom Bandopadhaya <shom@bandopadhaya.com>
2025-03-03 09:47:57 -0600

    Setting codeberg as 'primary' remote

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Personal site built on Hugo by Nix
 For local build run `./container/nix-container.sh`
+For local development with live reload run `./container/nix-local.sh`
--- setRemotes.sh
+++ setRemotes.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/sh
 set -eu
+git remote set-url --push origin --add git@codeberg.org:shom/shom.dev.git 
 git remote set-url --push origin --add git@git.sr.ht:~shom/shom.dev
 git remote set-url --push origin --add git@github.com:shombando/shom.dev.git
