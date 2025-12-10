ad4189e9051c0db82460bb722a4c17c579a103e4
Shom Bandopadhaya <shom@bandopadhaya.com>
2025-12-10 01:06:56 -0600

    Render only once with hugo even for local builds

--- build.sh
+++ build.sh
@@ -9,13 +9,16 @@ echo "==== Copy images to content folder ===="
 cp -r images/* content/
 echo ""
 
-echo "==== Build with Hugo ===="
-hugo
-echo ""
-
 echo "==== Copy .well-known for webfinger ===="
+mkdir -p ./public/
 cp ./.well-known -r ./public/
 echo ""
+
 if [[ "${1-production}"  == "develop" ]]; then
+		echo "==== Starting Hugo Development Server ===="
 		hugo server --bind 0.0.0.0 --buildDrafts --navigateToChanged --disableFastRender;
+else
+		echo "==== Build with Hugo ===="
+		hugo
+		echo ""
 fi
