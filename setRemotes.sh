881b80dab489989b140877d52b5f58c897355100
Shom Bandopadhaya <shom@bandopadhaya.com>
2026-08-08 12:36:45 -0500

    external forges as separate external remotes

--- setRemotes.sh
+++ setRemotes.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/sh
 set -eu
-git remote set-url --push origin --add git@codeberg.org:shom/shom.dev.git 
-git remote set-url --push origin --add git@git.sr.ht:~shom/shom.dev
-git remote set-url --push origin --add git@github.com:shombando/shom.dev.git
+git remote add external git@codeberg.org:shom/shom.dev.git
+git remote set-url --push external --add git@codeberg.org:shom/shom.dev.git 
+git remote set-url --push external --add git@git.sr.ht:~shom/shom.dev
+git remote set-url --push external --add git@github.com:shombando/shom.dev.git
